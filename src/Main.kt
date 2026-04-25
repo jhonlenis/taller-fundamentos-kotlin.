a63ced79cd33ff9cd85c446cb7d44a7a1a7bd680
jhonlenis <jhonalexander0606@gmail.com>
2026-04-24 22:07:42 -0500

    Archivo Main

---
+++ src/Main.kt
@@ -0,0 +1,15 @@
+// Archivo: Main.kt
+import ejercicios.*
+
+fun main() {
+    println("INICIANDO TALLER DE KOTLIN")
+
+    ejecutarBloque1()
+    ejecutarBloque2()
+    ejecutarBloque3()
+    ejecutarBloque4()
+    ejecutarBloque5()
+    ejecutarBloque6()
+    println("FIN DEL TALLER")
+
+}
